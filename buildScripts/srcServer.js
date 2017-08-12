268eba89808e0ab993df7181107d7292e451c741
chrisrymer <rymercho@gmail.com>
2017-08-12 09:27:48 +0100

    Add webpack dev and integrate with express

--- buildScripts/srcServer.js
+++ buildScripts/srcServer.js
@@ -1,9 +1,16 @@
 import express from 'express';
 import path from 'path';
 import open from 'open';
+import webpack from 'webpack';
+import config from '../webpack.config.dev.js';
 
 const port = 3000;
 const app  = express();
+
+app.use(require('webpack-dev-middleware', {
+  noInfo: true,
+  publicPath: config.output.publicPath
+}));
 
 app.get('/', function(req, res){
   res.sendFile(path.join(__dirname, '../src/index.html'));
